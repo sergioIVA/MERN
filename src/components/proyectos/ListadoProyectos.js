a0c490ac12b444c74334fd7c44fd5cca6c96814e
sergio <sergio.villamizar@mercadolibre.com.co>
2020-06-15 14:37:28 -0500

    first commit

---
+++ src/components/proyectos/ListadoProyectos.js
@@ -0,0 +1,33 @@
+import React,{useContext,useEffect} from 'react';
+import Proyecto from './Proyecto';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
+const ListadoProyectos = () => {
+
+      //Extraer proyectos de state inicial
+      const proyectosContext = useContext(proyectoContext);
+      const {proyectos,obtenerProyectos} =proyectosContext;
+
+      //Obtener proyectos cuando carga el componente
+      useEffect(()=>{
+        obtenerProyectos();
+     },[])
+
+     //revisar si proyectos tiene contenido
+     if(proyectos.length === 0) return null;
+
+     
+
+    return ( 
+        <ul className="listado-proyectos">
+            {proyectos.map(proyecto=>(
+                <Proyecto
+                    key={proyecto.id}
+                    proyecto={proyecto}
+                />
+            ))}
+        </ul>
+     );
+}
+ 
+export default ListadoProyectos;
---
+++ src/components/proyectos/NuevoProyecto.js
@@ -0,0 +1,78 @@
+import React,{Fragment,useState,useContext} from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
+const NuevoProyecto = () => {
+
+    //Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const {formulario,mostrarFormulario} =proyectosContext;
+
+    //State para el proyecto
+    const [proyecto,guardarProyecto] =useState({
+        nombre:''
+    });
+
+    //extraer nombre del proyecto
+    const {nombre} =proyecto;
+
+    //leer los contenidos del input
+    const onChangeProyecto=e=>{
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] :e.target.value
+        })
+    }
+
+    //Cuando el usuario envia un proyecto
+    const onSubmitProyecto=e=>{
+        e.preventDefault();
+
+        //Validar el proyecto
+
+        //agregar al state
+
+        //Reiniciar el form
+    }
+  
+    //Mostrar el formulario
+    const onClickFormulario =()=>{
+        mostrarFormulario();
+    }
+
+    return ( 
+        <Fragment>
+            <button
+          type="button"
+          className="btn btn-block btn-primario"
+          onClick={onClickFormulario}
+        >Nuevo Proyecto</button>
+          {
+             formulario ?
+             (
+                <form
+                className="formulario-nuevo-proyecto"
+                onSubmit={onSubmitProyecto}
+            >
+                <input
+                   type="text"
+                   className="input-text"
+                   placeholder="Nombre Proyecto"
+                   name="nombre"
+                   value={nombre}
+                   onChange={onChangeProyecto}
+                />
+                <input
+                    type="submit"
+                    className="btn btn-primario btn-block"
+                    value="Agregar Proyecto"
+                />
+            </form>
+             ) :null
+
+          }
+        </Fragment>
+        
+     );
+}
+ 
+export default NuevoProyecto;
